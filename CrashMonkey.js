9d892b8eab93553de5eb1b6e854c0e9f4d88eb19
Aniket Lawande <anlawande@gmail.com>
2016-01-26 10:06:32 -0500

    Installing dependencies. Doing everything sync for now

--- CrashMonkey.js
+++ CrashMonkey.js
@@ -1,5 +1,6 @@
 const del = require("del");
 const Promise = require("promise");
+const fs = require("fs");
 
 const argv = require('minimist')(process.argv.slice(2));
 const child_process = require("child_process");
@@ -13,30 +14,30 @@ if(!argv["url"] || !argv["test-engine"]) {
 }
 
 function cleanRepos() {
+	console.log("Cleaning Repos Directory");
 	del.sync(REPOS_PATH + "**", {force: true});
 	child_process.execSync("mkdir " + REPOS_DIR);
 }
 
 function getRepo(repoURL) {
-	return new Promise((resolve, reject) => {
-		child_process.exec('git clone ' + repoURL, {cwd: "repos"} , (error, stdout, stderr) => {
-			if (error !== null) {
-				console.log(`exec error: ${error}`);
-				process.exit(1);
-			}
-			resolve(/\/([^\/]+).git$/.exec(repoURL)[1]);
-		});
-	});
+	child_process.execSync('git clone ' + repoURL, {cwd: REPOS_DIR});
+	return /\/([^\/]+).git$/.exec(repoURL)[1];
 }
 
 function installDeps(repoName) {
+	var packageJSON = fs.readFileSync(REPOS_PATH + repoName + "/package.json", {encoding: "utf8"});
+	packageJSON = JSON.parse(packageJSON);
 	
+	console.log("Installing Dependencies");
+	var installRes = child_process.execSync("npm install", {cwd: REPOS_PATH + repoName});
+	console.log("Done");
 }
 
 function main() {
+	console.log("");
 	cleanRepos();
-	getRepo(argv["url"])
-		.then(installDeps);
+	var repoName = getRepo(argv["url"]);
+	installDeps(repoName);
 }
 
 main();
