568ada53aef7a294ba9ba17c8e726cb1fc4d6b63
Aniket Lawande <anlawande@gmail.com>
2016-02-04 23:41:50 -0500

    Get function for line number

--- tests/CodeParserTests.js
+++ tests/CodeParserTests.js
@@ -12,7 +12,7 @@ describe('Get Exported Functions', function() {
         assert.ok(_.some(funcs, func => func.name === "."));
         assert.ok(_.some(funcs, func => func.name === "abc"));
     });
-    
+
     it('should return function params', function () {
         var fileContents = fs.readFileSync("./tests/fixtures/module-exports.js", {encoding: "utf8"});
         var funcs = CodeParser.getAllExportedFunctions(esprima.parse(fileContents));
@@ -20,5 +20,19 @@ describe('Get Exported Functions', function() {
         assert.equal(1, funcs[0].params.length);
         assert.equal(2, funcs[2].params.length);
         assert.equal("mno", funcs[2].params[1].name);
+    });
+});
+
+describe('Get function for line number', function() {
+    it('should return outermost function for normal global functions', function () {
+        var fileContents = fs.readFileSync("./tests/fixtures/module-exports.js", {encoding: "utf8"});
+        var funcFound = CodeParser.getFuncForLine(fileContents, 1);
+        assert.equal("abc", funcFound.func.id.name);
+    });
+    
+    it('should return null if line number not within any function', function () {
+        var fileContents = fs.readFileSync("./tests/fixtures/module-exports.js", {encoding: "utf8"});
+        var funcFound = CodeParser.getFuncForLine(fileContents, 5);
+        assert.equal(null, funcFound);
     });
 });
