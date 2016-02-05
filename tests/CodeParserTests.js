2917ee9684b7cb68d3abc09927aa3403d847cb95
Aniket Lawande <anlawande@gmail.com>
2016-02-04 23:31:55 -0500

    Function params test

--- tests/CodeParserTests.js
+++ tests/CodeParserTests.js
@@ -12,4 +12,13 @@ describe('Get Exported Functions', function() {
         assert.ok(_.some(funcs, func => func.name === "."));
         assert.ok(_.some(funcs, func => func.name === "abc"));
     });
+    
+    it('should return function params', function () {
+        var fileContents = fs.readFileSync("./tests/fixtures/module-exports.js", {encoding: "utf8"});
+        var funcs = CodeParser.getAllExportedFunctions(esprima.parse(fileContents));
+        funcs = _.map(funcs, func => func["func"]);
+        assert.equal(1, funcs[0].params.length);
+        assert.equal(2, funcs[2].params.length);
+        assert.equal("mno", funcs[2].params[1].name);
+    });
 });
--- tests/fixtures/module-exports.js
+++ tests/fixtures/module-exports.js
@@ -1,9 +1,9 @@
-function abc() {
+function abc(def) {
     
 }
 
 module.exports = abc;
 module.exports.abc = abc;
-module.exports.abc = function() {
+module.exports.abc = function(xyz, mno) {
     
 };
