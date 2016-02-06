519cca961d89b1f8749e93aff40aa329a8a557f9
Aniket Lawande <anlawande@gmail.com>
2016-02-06 11:57:07 -0500

    getFuncsChanged for added files and signature changes

--- tests/OrchestratorTests.js
+++ tests/OrchestratorTests.js
@@ -11,13 +11,22 @@ describe('Orchestrator funcs changed tests', function() {
         assert.equal("getFuncForLine", files[0].funcs[0].name);
     });
     
-    it('should return functions for added files and signature changes', function () {
+    it('should return functions for signature changes', function () {
         var files = Orchestrator.getFuncsChanged(".", "HEAD", "24c1e5", "a188fe");
         assert.equal(3, files.length);
+        assert.equal(1, files[0].funcs.length);
+        assert.equal("getAllExportedFunctions", files[0].funcs[0].name);
+    });
+    
+    it('should return functions for added files', function () {
+        var files = Orchestrator.getFuncsChanged(".", "HEAD", "34fc4f", "53630f");
+        assert.equal(1, files.length);
+        assert.equal(1, files[0].funcs.length);
+        assert.equal("getChanges", files[0].funcs[0].name);
     });
     
     it('should not return functions for deleted files', function () {
         var files = Orchestrator.getFuncsChanged(".", "HEAD", "24c1e5", "a188fe");
-        assert.equal(3, files.length);
+        assert.equal(1, files.length);
     });
 });
