edd31ce2c7f6eed32ff51e7b5ea3dcc2131e708e
Aniket Lawande <anlawande@gmail.com>
2016-03-30 16:16:05 -0400

    Tracer modifying test files

--- tests/TracerTests.js
+++ tests/TracerTests.js
@@ -12,15 +12,27 @@ describe("Tracer tests", function () {
         if (!fs.existsSync("./repos/CrashMonkey"))
             child_process.execSync("git clone https://github.com/alt-code/CrashMonkey.git", {cwd: "./repos"});
         child_process.execSync("git checkout 1510a1", {cwd: "./repos/CrashMonkey"});
+        
+        var files = Orchestrator.getFuncsChanged("./repos/CrashMonkey");
+        //console.log(files.map(x => x.funcs));
+        Tracer.modifyFiles("./repos/CrashMonkey", files, ["tests/GitCenterTests.js"]);
     });
 
     it ("should augment function in file", function () {
-        var files = Orchestrator.getFuncsChanged("./repos/CrashMonkey");
-        //console.log(files.map(x => x.funcs));
-        Tracer.traceTestsForFilesFuncs("./repos/CrashMonkey", files, ["./repos/CrashMonkey/tests/GitCenterTests.js"]);
         var file = fs.readFileSync("./repos/CrashMonkey/lib/GitCenter.js", {encoding: "utf8"});
         var lines = file.split(/\r?\n/);
         var functionLine = _.findIndex(lines, x => x.indexOf("function getFileCommit") !== -1);
         assert.ok(lines[functionLine+1].indexOf("console.log('trace')") !== -1);
+    });
+    
+    it ("should augment all test files", function () {
+        var file = fs.readFileSync("./repos/CrashMonkey/tests/GitCenterTests.js", {encoding: "utf8"});
+        var lines = file.split(/\r?\n/);
+        var itLines = [];
+        _.each(lines, function(x, index) {
+            if (x.indexOf("it('should") !== -1)
+                 itLines.push(index);
+        });
+        assert.ok(_.every(itLines, x => /console\.log\('testcase \d+'\)/.test(lines[x+1]))) ;
     });
 });
